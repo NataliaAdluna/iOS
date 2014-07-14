fe27df2423420fa8582a1117a8660f29fca97baa
NataliaAdluna <natalia@adluna.pl>
2014-07-14 11:07:58 +0200

    First screen layout

---
+++ CombatZone/CombatZone/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  CombatZone
+//
+//  Created by Adluna on 14.07.2014.
+//  Copyright (c) 2014 Adluna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
