0de74674d32ccae3936759d8cc4f31f79c5665c8
NataliaAdluna <natalia@adluna.pl>
2014-07-14 11:46:27 +0200

    Button service

--- CombatZone/CombatZone/MainViewController.h
+++ CombatZone/CombatZone/MainViewController.h
@@ -9,5 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface MainViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *news;
+@property (weak, nonatomic) IBOutlet UIImageView *facebook;
+@property (weak, nonatomic) IBOutlet UIImageView *trainings;
+@property (weak, nonatomic) IBOutlet UIImageView *seminar;
+@property (weak, nonatomic) IBOutlet UIImageView *page;
+@property (weak, nonatomic) IBOutlet UIImageView *navi;
+@property (weak, nonatomic) IBOutlet UIImageView *play;
+@property (weak, nonatomic) IBOutlet UIImageView *foto;
 
 @end
--- CombatZone/CombatZone/MainViewController.m
+++ CombatZone/CombatZone/MainViewController.m
@@ -13,6 +13,54 @@
 @end
 
 @implementation MainViewController
+- (void) navi:(UIGestureRecognizer *)recognizer{
+    NSLog(@"jestem w navi");
+    //[self.tmplabel setText:[NSString stringWithFormat:@"trololo"]];
+    
+}
+
+- (void) play:(UIGestureRecognizer *)recognizer{
+    NSLog(@"jestem w play");
+    [self performSegueWithIdentifier:@"playSegue" sender:self];
+    //[self.tmplabel setText:[NSString stringWithFormat:@"trololo"]];
+    
+}
+
+- (void) foto:(UIGestureRecognizer *)recognizer{
+    NSLog(@"jestem w foto");
+    [self performSegueWithIdentifier:@"photoSegue" sender:self];
+    //[self.tmplabel setText:[NSString stringWithFormat:@"trololo"]];
+    
+}
+
+- (void) news:(UIGestureRecognizer *)recognizer{
+    NSLog(@"jestem w news");
+    [self performSegueWithIdentifier:@"newsSegue" sender:self];
+    //[self.tmplabel setText:[NSString stringWithFormat:@"trololo"]];
+    
+}
+- (void) facebook:(UIGestureRecognizer *)recognizer{
+    NSLog(@"jestem w facebook");
+    //[self.tmplabel setText:[NSString stringWithFormat:@"trololo"]];
+    
+}
+- (void) training:(UIGestureRecognizer *)recognizer{
+    NSLog(@"jestem w training");
+    [self performSegueWithIdentifier:@"trainingSegue" sender:self];
+    //[self.tmplabel setText:[NSString stringWithFormat:@"trololo"]];
+    
+}
+- (void) seminar:(UIGestureRecognizer *)recognizer{
+    NSLog(@"jestem w seminar");
+    [self performSegueWithIdentifier:@"seminarSegue" sender:self];
+    //[self.tmplabel setText:[NSString stringWithFormat:@"trololo"]];
+    
+}
+- (void) page:(UIGestureRecognizer *)recognizer{
+    NSLog(@"jestem w page");
+    //[self.tmplabel setText:[NSString stringWithFormat:@"trololo"]];
+    
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -26,7 +74,49 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    UITapGestureRecognizer *singleTapNavi= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(navi:)];
+    [singleTapNavi setNumberOfTapsRequired:1];
+    [self.navi setUserInteractionEnabled:YES];
+    [self.navi addGestureRecognizer:singleTapNavi];
+    
+    UITapGestureRecognizer *singleTapPlay= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(play:)];
+    [singleTapPlay setNumberOfTapsRequired:1];
+    [self.play setUserInteractionEnabled:YES];
+    [self.play addGestureRecognizer:singleTapPlay];
+    
+    UITapGestureRecognizer *singleTapPhoto= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(foto:)];
+    [singleTapPhoto setNumberOfTapsRequired:1];
+    [self.foto setUserInteractionEnabled:YES];
+    [self.foto addGestureRecognizer:singleTapPhoto];
+    
+    UITapGestureRecognizer *singleTapNews= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(news:)];
+    [singleTapNews setNumberOfTapsRequired:1];
+    [self.news setUserInteractionEnabled:YES];
+    [self.news addGestureRecognizer:singleTapNews];
+    
+    UITapGestureRecognizer *singleTapFacebook= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(facebook:)];
+    [singleTapFacebook setNumberOfTapsRequired:1];
+    [self.facebook setUserInteractionEnabled:YES];
+    [self.facebook addGestureRecognizer:singleTapFacebook];
+    
+    UITapGestureRecognizer *singleTapTraining= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(training:)];
+    [singleTapTraining setNumberOfTapsRequired:1];
+    [self.trainings setUserInteractionEnabled:YES];
+    [self.trainings addGestureRecognizer:singleTapTraining];
+    
+    UITapGestureRecognizer *singleTapSeminar= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(seminar:)];
+    [singleTapSeminar setNumberOfTapsRequired:1];
+    [self.seminar setUserInteractionEnabled:YES];
+    [self.seminar addGestureRecognizer:singleTapSeminar];
+    
+    UITapGestureRecognizer *singleTapPage= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(page:)];
+    [singleTapPage setNumberOfTapsRequired:1];
+    [self.page setUserInteractionEnabled:YES];
+    [self.page addGestureRecognizer:singleTapPage];
+    
+    
+
 }
 
 - (void)didReceiveMemoryWarning
